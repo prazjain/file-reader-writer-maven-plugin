063200a9149085b3b06c434a73f54e6fdaf36db3
Prashant Jain <praz.jain@gmail.com>
2025-02-28 12:53:52 -0500

    version updates

--- src/main/java/com/prazjain/FileProcessorMojo.java
+++ src/main/java/com/prazjain/FileProcessorMojo.java
@@ -1,21 +1,5 @@
 package com.prazjain;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.StringReader;
@@ -184,7 +168,7 @@ public class FileProcessorMojo
 
     private void checkYamlCorrectness(File inputFile) throws MojoExecutionException {
         try (FileInputStream fis = new FileInputStream(inputFile)) {
-            Yaml yaml = new Yaml(new SafeConstructor());
+            Yaml yaml = new Yaml();
             try {
                 yaml.load(fis);
             } catch (YAMLException e) {
